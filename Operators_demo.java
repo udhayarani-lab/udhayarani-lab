01aea2488467d8a7946e1958c676815259d3bbf8
udhayarani R <114601222+udhayarani-lab@users.noreply.github.com>
2022-10-02 19:59:18 -0700

    Add files via upload

---
+++ Operators_demo.java
@@ -0,0 +1,66 @@
+package datatype.revision;
+
+public class Operators_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//operators-arithmetic operator/relational operator/logical operators
+		     //arithmetic operators
+		//+-*/%
+		     int n1=10;
+		     int n2=20;
+		     
+		     System.out.println("addition.:"+(n1+n2));
+		     System.out.println("substraction..:"+(n1-n2));
+		     System.out.println("multiplication..:"+(n1*n2));
+		     System.out.println("division..quotiend..:"+(n1/n2));
+		     System.out.println("mod..reminder..:"+(n1%n2));
+		     
+		     //arimetic-lncrement and decrement
+		     
+		     n1=n1+1;
+		     n1=n1-1;
+		     
+		     n1=10;
+		     System.out.println(n1++);
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     System.out.println(++n1);
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     System.out.println(n1--);
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     System.out.println(--n1);
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     n1=n1+10;
+		     
+		     n1=10;
+		     n1+=10;
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     n1-=10;
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     n1*=10;
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     n1/=5;
+		     System.out.println(n1);
+		     
+		     n1=10;
+		     n1%=3;
+		     System.out.println(n1);
+		     
+
+	}
+
+}
